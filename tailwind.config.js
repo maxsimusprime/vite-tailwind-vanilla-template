86020aaf6ab38f37a52fa770cbf7683a7de32eba
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-06-10 18:58:26 +0300

    feat: implement custom container style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js}",
-    "./index.html"
-  ],
+  content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oswald: 'Oswald, sans-serif',
+      },
+    },
   },
   plugins: [],
-}
+  corePlugins: {
+    // Disabling default core `container` plugin
+    container: false,
+  },
+};
